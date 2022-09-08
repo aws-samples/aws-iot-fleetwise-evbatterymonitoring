eb2d4980267dde34b6cadc73b6b61c8222211c77
Andrei Svirida <svirida@amazon.de>
2022-09-08 22:27:58 +0200

    Update

--- simulatedvehicle/canreplay/service/start_simulation.sh
+++ simulatedvehicle/canreplay/service/start_simulation.sh
@@ -3,6 +3,7 @@ ROOTDIR=/home/ubuntu/aws-iot-fleetwise-evbatterymonitoring/simulatedvehicle/canr
 python3 $ROOTDIR/bin/can_replay.py \
           $ROOTDIR/config/ev/evdemo_tempissue.csv \
          --sleep-interval 1000 \
+         --delimiter ";" \
          --dbcfile $ROOTDIR/config/ev/evdemo.dbc \
          --obdconfig $ROOTDIR/config/ev/evdemo_obd_config.json
 
